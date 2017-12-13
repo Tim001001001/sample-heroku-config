96d5c774dcd08ff4bde2277d3a8f986d12c3e575
Tim001001001 <31822408+Tim001001001@users.noreply.github.com>
2017-12-13 17:09:58 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,31 @@
 from time import sleep
+API_KEY = "500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok"
+import requests
+import json
+
+URL = "https://api.telegram.org/bot" + API_KEY + "/getUpdates"
+print("URL:", URL, "\n")
+
+response = requests.get(URL) # make GET request
+updates = response.json() # parse python object from response
+
+print('Standard view:', updates, sep='\n')
+print()
+
+print('Pretty view:',
+      json.dumps(updates, indent=2, sort_keys=True),
+      sep='\n')
+from time import *
+
+last_update_id = 0
 
 while True:
-    print("Tick!")
-    sleep(2)
-    print("Tack!")
+    response = requests.get(URL)  # make GET request
+    updates = response.json()  # parse python object from response
+    last_update_id = last_update_id + 1
+    for msg in updates['result']:
+        print(1)
+
+        # requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
+
     sleep(1)
