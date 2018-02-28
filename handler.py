112efdb122dd2fdbde92d6c93dd217fc2814f2b8
Tim001001001 <31822408+Tim001001001@users.noreply.github.com>
2018-02-28 15:38:35 +0300

    Add files via upload

--- handler.py
+++ handler.py
@@ -1,29 +1,38 @@
+import datetime
 russianName = {"ru": "Русский язык ", "Ch": "Химия", "Mat": "Математика", "Phis": "Физика"}
 
 teacherName = {"ru": " с НЕКТО", "Ch": " c некто", "Mat": " с некто", "Phis": " с некто"}
-
-lessons = {"Понедельник": ["ru", "ru", 'Ch', 'Ch', 'Mat', 'Mat'],
-           "Вторник": ["ch", "Mat", 'Ch', 'Ch', 'Mat', 'Mat'],
-           "Среда": ["ru", "ru", 'hem', 'hem', 'ath', 'ath'],
-           "Четверг": ["Rs", "Rs", 'Cm', 'Cm', 'Mh', 'Mh'],
-           "Пятница": ["Rs", "Rs", 'Cm', 'Cm', "Mat", 'Mat']}
+daysofweek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
+lessons = [
+    ["ru", "ru", 'Ch', 'Ch', 'Mat', 'Mat'],
+    ["ch", "Mat", 'Ch', 'Ch', 'Mat', 'Mat'],
+    ["ru", "ru", 'hem', 'hem', 'ath', 'ath'],
+    ["Rs", "Rs", 'Cm', 'Cm', 'Mh', 'Mh'],
+    ["Rs", "Rs", 'Cm', 'Cm', "Mat", 'Mat']
+]
 
 time = ["9:00", "9:50", "10:40", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]
+weday = datetime.datetime.now()
 
 
 def handle_message(message, nickname=""):
 
     income = message.split()
     day = income[0]
-    if int(len(income)) == 1:
-        answer = lessons[day]
+
+    if day == "Сегодня":
+        answer = lessons[weday.weekday()]
     else:
+        day = daysofweek.index(income[0])
+        if int(len(income)) == 1:
+            answer = lessons[day]
+        else:
 
-        number_of_lesson = int(income[1]) - 1
+            number_of_lesson = int(income[1]) - 1
 
-        currentLesson = lessons[day][number_of_lesson]
+            currentLesson = lessons[day][number_of_lesson]
 
-        answer = russianName[currentLesson] + teacherName[currentLesson] + " в " + time[number_of_lesson]
+            answer = russianName[currentLesson] + teacherName[currentLesson] + " в " + time[number_of_lesson]
 
     return answer
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from time import sleep
+import datetime
 import requests
 import json
 from time import *
@@ -6,15 +6,10 @@ import handler
 API_KEY = "500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok"
 URL = "https://api.telegram.org/bot" + API_KEY + "/getUpdates"
 print("URL:", URL, "\n")
+weday = datetime.datetime.now()
 
 response = requests.get(URL)  # make GET request
 updates = response.json()  # parse python object from response
-
-# print('Standard view:', updates, sep='\n')
-#
-# print('Pretty view:',
-#       json.dumps(updates, indent=2, sort_keys=True),
-#       sep='\n')
 
 
 last_update_id = 0
@@ -34,57 +29,4 @@ while True:
         chat_id = msg["message"]["chat"]["id"]
         requests.post("https://api.telegram.org/bot" + API_KEY + "/sendMessage", params={"chat_id": chat_id, "text": ans})
 
-
-
-
-        # if (msg['message']['text']=="1"):
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': 'wwwww'
-        #         }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
-        # elif(msg['message']['text']=="2"):
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': " subject Li,teacherName D,roomNumber 201 subject Ch,teacherName EV,roomNumber 409 subject Math,teacherName EBroomNumber 206 "
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
-        # elif(msg['message']['text']=="3"):
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': " subject ,teacherName D,roomNumber 201 subject Ch,teacherName EV,roomNumber 409 subject Math,teacherName EBroomNumber 206 ",
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
-        # elif(msg['message']['text']=="4"):
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': " subject ru,teacherName D,roomNumber 201 subject Ch,teacherName EV,roomNumber 409 subject Math,teacherName EBroomNumber 206 ",
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
-        # elif(msg['message']['text']=="5"):
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': " subject __ teacherName D,roomNumber 201 subject Ch,teacherName EV,roomNumber 409 subject Math,teacherName EBroomNumber 206 ",
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
-        # elif(msg['message']['text']=="6"):
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': " no lessons ",
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage', params=data)
-        # elif (msg['message']['text'] == "7"):
-        #     data = {
-        #        'chat_id': 426464786,
-        #        'text': "  no lessons ",
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage',
-        #                  params=data)
-        # else:
-        #     data = {
-        #         'chat_id': 426464786,
-        #         'text': "  Error ",
-        #     }
-        #     requests.get('https://api.telegram.org/bot500594999:AAH5Io-AbkrluJUUAVali2EDSaYGFE1TQok/sendMessage',
-        #                  params=data)
     sleep(1)
